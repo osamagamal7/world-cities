b95ebf28d75a187ebcaa1d1ba160e572f0d32df5
osama <osama.abbas@nia-medtech.com>
2022-12-05 05:13:55 +0100

    tile component for post

--- src/components/post/PostTile.tsx
+++ src/components/post/PostTile.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CustomButton } from "../Button/Button.style";
+import { CustomImage, InnerPostTile, GridContainer, TextBody, Title } from "./PostTile.style";
 
 type MainPostProps = {
     content: string;
@@ -9,10 +12,39 @@ type MainPostProps = {
 };
 
 export const PostTile: React.FC<MainPostProps> = ({
-
+    content,
+    image_url,
+    id,
+    removePost,
+    title
 }) => {
+    const navigate = useNavigate();
 
     return (
-        <></>
+        <GridContainer
+        >
+            <InnerPostTile>
+                <ul style={{}}>
+                    <li>
+                        <CustomImage src={image_url} />
+                    </li>
+                    <li>
+                        <Title>{title}</Title>
+                    </li>
+                    <li>
+                        <TextBody>{content}</TextBody>
+                    </li>
+                </ul>
+                <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+                    <CustomButton onClick={() => navigate(`post-details/${id}`)}>View</CustomButton>
+                    <CustomButton
+                        dangerBackground
+                        onClick={() => removePost(id)}
+                    >
+                        Delete
+                    </CustomButton>
+                </div>
+            </InnerPostTile>
+        </GridContainer >
     );
 };
