286cc42dcd0ad073fcc0e8fbf7426fbfc107683a
osama <osama.abbas@nia-medtech.com>
2022-12-05 04:40:20 +0100

    add basic theming

---
+++ src/theme/theme.ts
@@ -0,0 +1,14 @@
+type themeType = {
+    primary: string;
+    secondary: string;
+    white: string;
+    danger: string;
+    dangerExtra: string;
+}
+export const theme: themeType = {
+    primary: "#501f96",
+    secondary: "#341454",
+    white: "#fff",
+    danger: "#DC3545",
+    dangerExtra: "#AF0606",
+}
