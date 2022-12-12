24a037e45c1dde6e569b3c63fb180d0608e2e196
osama <osama.abbas@nia-medtech.com>
2022-12-12 10:47:23 +0100

    update UI

--- src/components/Button/Button.style.ts
+++ src/components/Button/Button.style.ts
@@ -10,15 +10,14 @@ export const CustomButton = styled.button`
   font-weight: 600;
   background-color: ${(props: CustomButtonType) =>
     props.dangerBackground ? theme.danger : theme.primary};
+  text-transform: uppercase;
   border-radius: 20px;
   color: ${theme.white};
   padding: 10px 20px;
-  text-align: center;
-  display: inline;
   font-size: 16px;
   cursor: pointer;
   transition: 0.5s all ease-out;
-  margin-top: 10px;
+  margin: 10px 0;
   width: 30%;
 
   &:hover {
